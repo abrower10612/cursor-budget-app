53c1439528e323911ef2e1c3e5f5a78e2e72d003
Andrew Brower <abrower10612@gmail.com>
2025-02-28 21:40:34 -0700

    update build script with Linux dependencies

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 # exit on error
 set -o errexit
 
+# Install required dependencies
+apt-get update && apt-get install -y curl git unzip xz-utils zip libglu1-mesa chromium
+
 # Download Flutter SDK
 git clone https://github.com/flutter/flutter.git
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Use Chromium instead of Chrome
+export CHROME_EXECUTABLE=$(which chromium)
 
 # Install Flutter
 flutter precache
